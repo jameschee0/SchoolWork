224b418fc40ada2b734a4f594c4e00e93e3a8eee
James Chee <jamescheeinnovation@gmail.com>
2017-11-18 17:31:38 +0900

    primary work on triangle.java

---
+++ triangle.java
@@ -0,0 +1,21 @@
+/*
+Java Program
+Drawing Triangle
+Created by James Chee 2017.11.18
+*/
+
+import java.util.Scanner;
+
+public class triangle{
+	public static void main(String[] args){
+
+	}
+
+	public static void space(){
+		System.out.print(" ");
+	}
+
+	public static void star(){
+		System.out.print("*");
+	}
+}
