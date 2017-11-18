604c9cafc564ce7e256551d59d47359d0803642a
James Chee <jamescheeinnovation@gmail.com>
2017-11-18 23:21:11 +0900

    major changes to triangle. new triangle class file

--- triangle.java
+++ triangle.java
@@ -8,7 +8,17 @@ import java.util.Scanner;
 
 public class triangle{
 	public static void main(String[] args){
-		
+		Scanner scan = new Scanner(System.in);
+		int in;
+		in = scan.nextInt();
+		if(in%2==0){
+			in -= 1;
+		}
+		for(int i=1;(2*i-1)<=in;i++){
+			for(int j=1;j<=(in-i);j++)space();
+			for(int j=1;j<=(2*i-1);j++)star();
+			next();
+		}
 	}
 
 	public static void space(){
@@ -17,5 +27,9 @@ public class triangle{
 
 	public static void star(){
 		System.out.print("*");
+	}
+
+	public static void next(){
+		System.out.println();
 	}
 }
